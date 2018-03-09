12614911c827d1148c7a68a564c10d99a64dc29d
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-08 18:32:08 -0800

    testing authentication

--- web_server/src/db.py
+++ web_server/src/db.py
@@ -38,7 +38,7 @@ def new_User(user_id, password):
 	t.save()
 
 def get_User_pass(user_id):
-	return Auth.select().where(Auth.user_id == user_id).password
+	return Auth.select().where(Auth.user_id == user_id)[0].password
 
 def create_tables():
     with db:
