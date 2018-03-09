49e8c5555e6d01e9fa829b950af33fcd1bf5fe28
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-08 18:22:16 -0800

    testing authentication

--- web_server/src/app.py
+++ web_server/src/app.py
@@ -48,7 +48,7 @@ def new_user():
 		if password:
 
 			hashed_password = hash_password(password)
-			new_User(user_id, hash_password)
+			new_User(username, hash_password)
 
 			return "New User added"
 		else:
