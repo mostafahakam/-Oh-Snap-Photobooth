6fc793cd11d2ea1afc7859af792ff6969e01029e
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-20 13:58:06 -0700

    adding upload to ig

--- web_server/src/app.py
+++ web_server/src/app.py
@@ -8,6 +8,7 @@ import numpy as np
 import os
 from werkzeug.utils import secure_filename
 import hashlib
+from InstagramAPI import InstagramAPI
 
 from db import *
 from auth import *
@@ -19,7 +20,7 @@ UPLOAD_FOLDER = '/var/www/static/img'
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+api = InstagramAPI("ohsnap_391", "ohsnap_391pass")
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -165,9 +166,22 @@ def ret_images(user_id):
 
 @app.route('/upload_to_instagram/<filename>', methods=['POST'])
 def post_to_ig(filename):
-    upload_to_Instagram(self, filename)
+    upload_to_Instagram(filename)
 
     return "Success"
+
+
+def upload_to_Instagram(filename):
+    if api.login():
+        api.getSelfUserFeed()  # get self user feed
+        print(api.LastJson)  # print last response JSON
+        print("Login succes!")
+    else:
+        print("Can't login!")
+
+    photo_path = '/var/www/static/img/' + filename
+    caption = "Testing"
+    InstagramAPI.uploadPhoto(photo_path, caption=caption)
 
 
 def detect_faces_in_image(file_stream):
--- web_server/src/social_media.py
+++ web_server/src/social_media.py
@@ -1,16 +1,4 @@
-from InstagramAPI import InstagramAPI
-
-api = InstagramAPI("ohsnap_391", "ohsnap_391pass")
 
 
-def upload_to_Instagram(self, filename):
-    if api.login():
-        api.getSelfUserFeed()  # get self user feed
-        print(api.LastJson)  # print last response JSON
-        print("Login succes!")
-    else:
-        print("Can't login!")
 
-    photo_path = '/var/www/static/img/' + filename
-    caption = "Testing"
-    InstagramAPI.uploadPhoto(self=self, photo=photo_path, caption=caption)
+
