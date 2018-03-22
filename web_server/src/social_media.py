25e7d899daab961aa8153c4861b399d8faf50e7a
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-22 16:16:31 -0700

    social

--- web_server/src/social_media.py
+++ web_server/src/social_media.py
@@ -1,7 +1,7 @@
 from InstagramAPI import InstagramAPI
-from PIL import Image
 
-api = InstagramAPI("cpen391_ohsnap", "ohsnap_pass")
+
+api = InstagramAPI("ohsnap_391", "ohsnap_391pass")
 
 
 def upload_to_Instagram(filename):
@@ -19,9 +19,6 @@ def upload_to_Instagram(filename):
 
     print("Is logged in: " + str(api.isLoggedIn))
     photo_path = '/var/www/static/img/' + filename
-    img = Image.open(photo_path)
 
-    img.save('instagram/'+filename.split(".")[0], 'jpg')
     caption = "Testing"
-
-    api.uploadPhoto('instagram/'+filename.split(".")[0] + '.jpg', caption=caption)
+    api.uploadPhoto(photo_path, caption=caption)
