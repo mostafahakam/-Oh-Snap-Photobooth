b1db415046c5de4f49575babd899cc6c319d1578
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-22 14:27:49 -0700

    fixed social media upload

--- web_server/src/social_media.py
+++ web_server/src/social_media.py
@@ -5,16 +5,20 @@ api = InstagramAPI("ohsnap_391", "ohsnap_391pass")
 
 def upload_to_Instagram(filename):
 
-    if api.login():
-        api.getSelfUserFeed()  # get self user feed
-        print(api.LastJson)  # print last response JSON
-        print("Login succes!")
+    if not api.isLoggedIn():
 
-        photo_path = '/var/www/static/img/' + filename
-        caption = "Testing"
-        api.uploadPhoto(photo_path, caption=caption)
-        api.logout()
+        if api.login():
+            api.getSelfUserFeed()  # get self user feed
+            print(api.LastJson)  # print last response JSON
+            print("Login succes!")
+
+        else:
+            print("Can't login!")
+            return
+
+    photo_path = '/var/www/static/img/' + filename
+    caption = "Testing"
+    api.uploadPhoto(photo_path, caption=caption)
+    api.logout()
 
 
-    else:
-        print("Can't login!")
