bc05edeb3aab9f9cf23c1996b41a03933a0e933f
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-27 16:04:46 -0700

    social

--- web_server/src/social_media.py
+++ web_server/src/social_media.py
@@ -20,5 +20,23 @@ def upload_to_Instagram(filename):
     print("Is logged in: " + str(api.isLoggedIn))
     photo_path = '/var/www/static/img/' + filename
 
+    media = [
+        {
+            'type': 'photo',
+            'file': 'logo.jpg',  # Path to the photo file.
+        },
+        {
+            'type': 'photo',
+            'file': photo_path,  # Path to the photo file.
+            'usertags': [
+                {  # Optional, lets you tag one or more users in a PHOTO.
+                    'position': [0.5, 0.5],
+                    # WARNING: THE USER ID MUST BE VALID. INSTAGRAM WILL VERIFY IT
+                    # AND IF IT'S WRONG THEY WILL SAY "media configure error".
+                    'user_id': '123456789',  # Must be a numerical UserPK ID.
+                },
+            ]
+        },
+    ]
     caption = "Testing"
-    api.uploadPhoto(photo_path, caption=caption)
+    api.uploadAlbum(media, caption=caption)
