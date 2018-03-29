cc9ec72ce6d4472b9456510886c2cd368db2b08b
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-29 15:03:02 -0700

    social

--- web_server/src/social_media.py
+++ web_server/src/social_media.py
@@ -20,11 +20,7 @@ def upload_to_Instagram(filename):
     print("Is logged in: " + str(api.isLoggedIn))
     photo_path = '/var/www/static/img/' + filename
 
-    media = [
-        {
-            'type': 'photo',
-            'file': 'logo.jpg',  # Path to the photo file.
-        },
+    media = [  # Albums can contain between 2 and 10 photos/videos.
         {
             'type': 'photo',
             'file': photo_path,  # Path to the photo file.
@@ -33,10 +29,19 @@ def upload_to_Instagram(filename):
                     'position': [0.5, 0.5],
                     # WARNING: THE USER ID MUST BE VALID. INSTAGRAM WILL VERIFY IT
                     # AND IF IT'S WRONG THEY WILL SAY "media configure error".
-                    'user_id': '536372018',  # Must be a numerical UserPK ID.
+                    'user_id': '123456789',  # Must be a numerical UserPK ID.
                 },
             ]
         },
+        {
+            'type': 'photo',
+            'file': 'logo.jpg',  # Path to the photo file.
+        },
+        # {
+        #    'type'     : 'video',
+        #    'file'     : '/path/to/your/video.mp4', # Path to the video file.
+        #    'thumbnail': '/path/to/your/thumbnail.jpg'
+        # }
     ]
     caption = "Testing"
     api.uploadAlbum(media, caption=caption)
