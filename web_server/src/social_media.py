2573a65de04ccb3e160d5f9eb86a5cc0692f5c57
Mostafa Hakam <mostafahakam@hotmail.com>
2018-03-29 17:00:18 -0700

    social

--- web_server/src/social_media.py
+++ web_server/src/social_media.py
@@ -28,9 +28,11 @@ def upload_to_Instagram(ig, filename):
     face_locations = face_recognition.face_locations(image)
     face_encodings = face_recognition.face_encodings(image, face_locations)
 
-    for location in face_locations:
-        print(location)
+    #for location in face_locations:
+    #    print(location)
 
+    print(getImageSize(photo_path))
+    
     media = [
         {
             'type': 'photo',
@@ -49,3 +51,34 @@ def upload_to_Instagram(ig, filename):
     ]
     caption = "Testing"
     api.uploadAlbum(media, caption=caption)
+
+
+def getImageSize(fname):
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            raise RuntimeError("Invalid Header")
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                raise RuntimeError("PNG: Invalid check")
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            fhandle.seek(0)  # Read 0xff next
+            size = 2
+            ftype = 0
+            while not 0xc0 <= ftype <= 0xcf:
+                fhandle.seek(size, 1)
+                byte = fhandle.read(1)
+                while ord(byte) == 0xff:
+                    byte = fhandle.read(1)
+                ftype = ord(byte)
+                size = struct.unpack('>H', fhandle.read(2))[0] - 2
+            # We are at a SOFn block
+            fhandle.seek(1, 1)  # Skip `precision' byte.
+            height, width = struct.unpack('>HH', fhandle.read(4))
+        else:
+            raise RuntimeError("Unsupported format")
+        return width, height
